28aec8e108b2ed97a804758f8107d2f58d5d2a74
alithethird <39213991+alithethird@users.noreply.github.com>
2021-01-29 17:33:31 +0300

    demo code written with signal for timer interrupt

--- motor_demo.py
+++ motor_demo.py
@@ -1,5 +1,13 @@
-import motor_driver
+import time
+import signal
 
-md = motor_driver.motor_driver()
 
-md.motor_run(0.001, 400, 1)
+def catcher(signum, _):
+    print("yeah")
+
+
+signal.signal(signal.SIGALRM, catcher)
+signal.setitimer(signal.ITIMER_REAL, 0.01, 3)
+
+while True:
+    time.sleep(5)
