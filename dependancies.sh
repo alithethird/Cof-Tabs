b64f6073df4223e2ac575b417155a90c4dc2541a
alithethird <aliugur997@gmail.com>
2021-01-30 15:37:43 +0300

    mpu6050 ilk kodu eklendi

--- dependancies.sh
+++ dependancies.sh
@@ -5,3 +5,8 @@ sudo apt install python3-pyqt5 -y
 sudo apt install python3-pip -y
 
 pip3 install pyqtgraph
+
+echo "mpu6050 dependancies:"
+
+sudo apt install python-smbus
+sudo pip install mpu6050
