bfe6871b614dec213e758029524151299ad1e2e1
alithethird <solabulldoser@gmail.com>
2021-02-16 18:10:11 +0300

    dependancies.sh güncellendi

--- dependancies.sh
+++ dependancies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "updating repositories"
+
+sudo apt update
+
 echo "pip3 installing"
 
 sudo apt install python3-pip -y
@@ -11,6 +15,16 @@ pip3 install fpdf2
 echo "kivy installing"
 
 python3 -m pip install kivy[base]
+
+echo "kivy dependancies"
+sudo apt install pkg-config libgl1-mesa-dev libgles2-mesa-dev \
+   libgstreamer1.0-dev \
+   gstreamer1.0-plugins-{bad,base,good,ugly} \
+   gstreamer1.0-{omx,alsa} libmtdev-dev \
+   xclip xsel libjpeg-dev
+
+echo "sdl2 installing"
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
 
 echo "kivy-garden installing"
 
